03dc80f576c4585478fba049ad31d3b270a1f3be
HP <mahletabateabate2@gmail.com>
2022-11-09 16:15:21 -0500

    ryan add

--- airport-Search/src/main/java/com/airportSearch/airportSearch/Model/Flights.java
+++ airport-Search/src/main/java/com/airportSearch/airportSearch/Model/Flights.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name = "Flights")
 public class Flights {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
     private String departure_City;
