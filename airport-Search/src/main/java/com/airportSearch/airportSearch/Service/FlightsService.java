436172325cca7c7f37f2c5f250ceca36697c62ab
Ryan Brooks <RJBComputerScientist@gmail.com>
2022-11-09 16:08:38 -0500

    Added more functionaility To DAO Pattern

--- airport-Search/src/main/java/com/airportSearch/airportSearch/Service/FlightsService.java
+++ airport-Search/src/main/java/com/airportSearch/airportSearch/Service/FlightsService.java
@@ -3,25 +3,43 @@ package com.airportSearch.airportSearch.Service;
 import com.airportSearch.airportSearch.DAO.CitiesRepository;
 import com.airportSearch.airportSearch.DAO.FlightsRepository;
 import com.airportSearch.airportSearch.Model.Flights;
+import com.airportSearch.airportSearch.repositories.CitiesRepo;
+import com.airportSearch.airportSearch.repositories.FlightsRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 @Component
 
 public class FlightsService {
     @Autowired
     CitiesRepository citiesRepository;
     FlightsRepository flightsRepository;
+    
+    @Autowired
+    CitiesRepo citiesrepo;
+    FlightsRepo flightsrepo;
 
     public FlightsService (CitiesRepository citiesRepository, FlightsRepository flightsRepository){
         this.citiesRepository = citiesRepository;
         this.flightsRepository = flightsRepository;
     }
-    public  List <Flights> getAllFlights() {
+    public List <Flights> getAllFlights() {
 
         return flightsRepository.findAll();
 
+    }
+    
+    public Flights findByFlightId(int flightID) {
+    	Optional<Flights> optional = flightsrepo.findById(flightID);
+    	
+    	if(!optional.isPresent()) {
+    		return null;
+    	} else {
+    		return optional.get();
+    	}
     }
     public  List <Flights> getFlightByDepartureCity(String name){
         return  flightsRepository.getFlightByDepartureCity(name);
---
+++ airport-Search/src/main/java/com/airportSearch/airportSearch/repositories/CitiesRepo.java
@@ -0,0 +1,15 @@
+package com.airportSearch.airportSearch.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.airportSearch.airportSearch.Model.Cities;
+
+@Repository
+public interface CitiesRepo extends JpaRepository<Cities, Integer>{
+	Optional<Cities> findByCity(int CityNumber);
+	
+	Cities findByName(String CityName);
+}
